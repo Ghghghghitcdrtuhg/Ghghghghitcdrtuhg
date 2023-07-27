08553b9e1095ce3c86896df0f802ee87796db622
Ghghghghitcdrtuhg <140763974+Ghghghghitcdrtuhg@users.noreply.github.com>
2023-07-27 18:01:57 -0500

    Skywars hacks

---
+++ README.m
@@ -0,0 +1,71 @@
+--- hub
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))() ---- you can edit this script if you now how to script
+local Window = Library.CreateLib("roblox hacks", "Synapse")
+-- main
+local Main = Window:NewTab("roblox hacks main") ---- dont not edited this part at all and all the down one the up you can edit if you know how to script
+local MainSection = Main:NewSection("Section 1")
+MainSection:NewButton("admin but you dont have admin", "admin but yo dont have admin", function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
+MainSection:NewSlider("super speed", "you go brrr", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+MainSection:NewSlider("super Jump", "you go to high", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+end)
+MainSection:NewButton("better chat bypass", "chat bypass use it careful it can be patched", function() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/daddysyn/synergy/additional/betterbypasser",true))()
+end)
+MainSection:NewButton("Old skywars script", "broken", function()
+loadstring(game:HttpGethttps("://paste.ee/r/r9gnA", true))()
+end)
+
+
+--Player
+local Player = Window:NewTab("Local player script")
+local PlayerSection = Player:NewSection("Player script")
+PlayerSection:NewToggle("super human", "go fast and jump high", function(state)
+    if state then
+        game.Players.localPlayer.Character.Humanoid.WalkSpeed = 120
+        game.Players.localPlayer.Character.Humanoid.JumpPower = 120 
+		else
+        game.Players.localPlayer.Character.Humanoid.WalkSpeed = 16
+        game.Players.localPlayer.Character.Humanoid.JumpPower = 50
+	end      
+        end)
+		PlayerSection:NewButton("reset your speed", "use this if the super human got bug and cant slow down", function()
+        game.Players.localPlayer.Character.Humanoid.WalkSpeed = 16
+        game.Players.localPlayer.Character.Humanoid.JumpPower = 50
+end)
+
+
+--- others 
+local Others = Window:NewTab("Others scripts")
+local OthersSection = Others:NewSection("Others random scripts coming soon")
+OthersSection:NewButton("chat spoofer", "its that robblox talking? chat hax", function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/ant-7802/--/main/straightmilk.lua'))()
+end)
+OthersSection:NewButton("broken script", "broken fly bypass", function()
+loadstring(game:HttpGet('https://pastebin.com/fPtT2Q1F', true))()
+end)
+
+
+
+
+
+
+-- Skywars
+local Skywars = Window:NewTab("Skywars")
+local SkywarsSection = Skywars:NewSection("hubs")
+SkywarsSection:NewButton("hub", "Skywars hub", function()
+loadstring(game:HttpGet("https://uraniumhub.tk/storage/script.lua", true))()
+end)
+
+
+
+
+
+
+
+
+
